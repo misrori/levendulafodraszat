974194bbbaaff5580b41d9e755b8e4ada781c7e1
Mihály Orsós <ormraat.pte@gmail.com>
2026-01-31 20:08:30 +0100

    b

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/levendulafodraszat">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/szolgaltatasok" element={<Szolgaltatasok />} />
